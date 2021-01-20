dbe92aabfde59f4657d7940d82a1b9949bb8b68e
Aslı BİRBİLEN <a.brbln.97@gmail.com>
2021-01-20 16:50:38 +0300

    Entities...

---
+++ CarGalery.Enitities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Enitities.Abstract
+{
+    public interface IEntity
+    {
+    }
+}
---
+++ CarGalery.Enitities/Concrete/Car.cs
@@ -0,0 +1,31 @@
+﻿using CarGalery.Enitities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Enitities.Concrete
+{
+    public class Car :IEntity
+    {
+        
+        //en az üç alan
+        public int CarId { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+
+        }
+        public Car(string model, decimal price)
+        {
+            Model = model;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"{CarId,-5} {Model,-30} {Price,-10}";
+        }
+
+    }
+}
