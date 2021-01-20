ca09dfbff2b3a803242ed1663f82b4891e461f47
Aslı BİRBİLEN <a.brbln.97@gmail.com>
2021-01-20 17:57:58 +0300

    Data access layer has been implemented.

---
+++ CarGalery.DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,12 @@
+﻿using CarGalery.Enitities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.DataAccess.Abstract
+{
+    public interface ICarDal : IEntityRepository<Car>
+    {
+
+    }
+}
